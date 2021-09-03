712e0233d1aa6ab7c5706bc7286866bc9a26e7ba
Ruslan Khamidullin <ruslan.khamidullin@toptal.com>
2021-09-03 21:09:21 +0300

    [MONGODB] connected to Mongo via Mongoose and dotenv

--- server/app.js
+++ server/app.js
@@ -1,15 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3005;
+
+mongoose.connect(`mongodb+srv://${process.env.M_USER}:${process.env.M_PASS}@testcluster.toqac.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
 
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.on('open', () => console.log(`Connected to DB!`));
+
 app.listen(PORT, err => {
   err ? console.log(error) : console.log('Server started');
-})
+});
